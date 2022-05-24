1e877adb4ac930828b5fe8b61746faeea3d933e8
Felipe Rodrigues <rodfelipee1@gmail.com>
2022-05-24 09:47:01 -0300

    all graphics working

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 # Imports
-import os
-import sys
 import numpy as np
-import skfuzzy as fz
-
-# GPUs dic
-gpu = {
-    'GTX 1080ti': {
-        'RAM': '11gb',
-        'CLOCK': '1481Mhz',
-        'BUS': '352 bits',
-        'PRICE': 'R$ 3.000,00',
-        'YEAR': 2017
-    },
-    'RTX 2060': {
-        'RAM': '6gb',
-        'CLOCK': '1365Mhz',
-        'BUS': '192 bits',
-        'PRICE': 'R$ 2.700,00',
-        'YEAR': 2020
-    }
-}
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
 
 
 # Main Class
 class Main():
     def __init__(self):
-        for a, b in gpu.items():
-            print(f'\nPlaca: {a}')
-            specs = '      -> RAM: ' + str(b['RAM']) + '\n'
-            specs += '      -> BUS: ' + str(b['BUS']) + '\n'
-            specs += '      -> CLOCK: ' + str(b['CLOCK']) + '\n'
-            specs += '      -> PRICE: ' + str(b['PRICE']) + '\n'
-            specs += '      -> YEAR: ' + str(b['YEAR']) + '\n'
-            print(f'    Specs:\n{specs}')
 
+        # Variables
+        temp = np.arange(0, 85, 1)  # temperature
+        usage = np.arange(0, 100, 1)  # usage
+        per = np.arange(0, 100, 1)  # performance
+
+        # temperature sets
+        temp_low = fuzz.trimf(temp, [0, 0, 15])
+        temp_avg = fuzz.trimf(temp, [0, 15, 50])
+        temp_high = fuzz.trimf(temp, [15, 50, 85])
+
+        # usage sets
+        usage_low = fuzz.trimf(usage, [0, 0, 0])
+        usage_avg = fuzz.trimf(usage, [0, 0, 50])
+        usage_high = fuzz.trimf(usage, [0, 50, 100])
+
+        # usage sets
+        per_low = fuzz.trimf(per, [0, 0, 0])
+        per_avg = fuzz.trimf(per, [0, 0, 50])
+        per_high = fuzz.trimf(per, [0, 50, 100])
+
+        fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 9))
+
+        ax0.plot(temp, temp_low, 'b', linewidth=1.5, label='Cold')
+        ax0.plot(temp, temp_avg, 'g', linewidth=1.5, label='Reasonable')
+        ax0.plot(temp, temp_high, 'r', linewidth=1.5, label='Bad')
+        ax0.set_title('Temperature')
+        ax0.legend()
+
+        ax1.plot(usage, usage_low, 'b', linewidth=1.5, label='Low')
+        ax1.plot(usage, usage_avg, 'g', linewidth=1.5, label='Medium')
+        ax1.plot(usage, usage_high, 'r', linewidth=1.5, label='High')
+        ax1.set_title('Usage')
+        ax1.legend()
+
+        ax2.plot(per, per_low, 'r', linewidth=1.5, label='Bad')
+        ax2.plot(per, per_avg, 'b', linewidth=1.5, label='Average')
+        ax2.plot(per, per_high, 'g', linewidth=1.5, label='High')
+        ax2.set_title('Performance')
+        ax2.legend()
+
+
+        plt.show()
 
 if __name__ == '__main__':
     Main()
